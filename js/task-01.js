d85eeab6eaca6d70e17f91a8a01565f9de204ffa
vadik184 <133372327+vadik184@users.noreply.github.com>
2024-03-24 19:56:28 +0200

    made prompt

--- js/task-01.js
+++ js/task-01.js
@@ -4,21 +4,44 @@ const deleteButton = document.querySelector(".delete");
 const colorChangeButton = document.querySelector(".color-change");
 const colorOptions = document.querySelector(".color-options");
 
-editButton.addEventListener("click", () => {
-  const newTitle = prompt(
-    "Enter new title:",
-    todoCard.querySelector("h3").textContent
-  );
+const customPrompt = document.getElementById("customPrompt");
+const newTitleInput = document.getElementById("newTitleInput");
+const submitPromptButton = document.getElementById("submitPrompt");
 
-  if (newTitle !== null) {
-    todoCard.querySelector("h3").textContent = newTitle;
+// editButton.addEventListener("click", () => {
+//   const newTitle = prompt(
+//     "Enter new title:",
+//     todoCard.querySelector("h3").textContent
+//   );
+
+//   if (newTitle !== null) {
+//     todoCard.querySelector("h3").textContent = newTitle;
+//   }
+// });
+
+editButton.addEventListener("click", () => {
+  if (todoCard.querySelector("h3").textContent === "Title") {
+    newTitleInput.value = "";
+  } else {
+    newTitleInput.value = todoCard.querySelector("h3").textContent;
   }
+
+  customPrompt.style.display = "block";
 });
 
 deleteButton.addEventListener("click", () => {
   todoCard.remove();
 });
 
+submitPromptButton.addEventListener("click", () => {
+  const newTitle = newTitleInput.value.trim();
+  if (newTitle !== "") {
+    todoCard.querySelector("h3").textContent = newTitle;
+    customPrompt.style.display = "none";
+  } else {
+    alert("Please enter both a new title and a new description.");
+  }
+});
 colorChangeButton.addEventListener("click", () => {
   colorOptions.style.display = "flex";
 });
